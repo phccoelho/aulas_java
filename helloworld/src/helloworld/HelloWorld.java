93c157c6b9bbb67cd11ef200a40b307f49a56d3f
Pedro Coelho <phcoelho2004@gmail.com>
2025-04-11 11:15:44 -0300

    :tada: Commit Inicial

---
+++ helloworld/src/helloworld/HelloWorld.java
@@ -0,0 +1,11 @@
+package helloworld;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World!");
+		System.out.println("A turma Java 82 é top");
+	}
+
+}
